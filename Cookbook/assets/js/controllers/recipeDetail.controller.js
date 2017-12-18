5c1e39adc4650f4b0ffdad0f0ecdd112230d175b
Cédric DANIEL <cdanie@cfcci53.fr>
2017-12-18 14:54:33 +0100

    Prise en compte du service OpenFoodFacts dans le détail de la recette

--- Cookbook/assets/js/controllers/recipeDetail.controller.js
+++ Cookbook/assets/js/controllers/recipeDetail.controller.js
@@ -1,6 +1,7 @@
-app.controller('recipeDetail', function($scope, recipeSvc){
+app.controller('recipeDetail', function($scope, recipeSvc, openfoodfactSvc){
     /* recette en cours d'édition */
     $scope.recipe = undefined;
+    $scope.openFoodFactsResult = undefined;
     
     /**
      * Interroge le service recipeSvc pour récupérer la recette qui a été sélectionnée
@@ -12,5 +13,50 @@ app.controller('recipeDetail', function($scope, recipeSvc){
     
     $scope.$watch(checkRecipe, function(newValue, oldValue, scope){
         $scope.recipe = newValue;
-    })
+    });
+    
+    /**
+     * Interroge le service openfoodfacts
+     * @param {string} input Saisie de l'utilisateur
+     */
+    $scope.searchIngredient = function(input){
+        $scope.openFoodFactsResult = [];
+        
+        openfoodfactSvc
+            .search(input)
+            .then(function(response){
+                // Cette fonction sera appelée en cas de succès de la requête
+                console.log(response);
+                for(var i = 0; i<response.data.products.length; i++){
+                    var item = response.data.products[i];
+                    
+                    // Recomposition d'un objet ingrédient à partir d'item
+                    var monIngredient = {
+                        name:item.product_name_fr,
+                        brand:item.brands
+                    };
+                    
+                    // Ajout de l'objet au tableau
+                    $scope.openFoodFactsResult.push(monIngredient);
+                }
+            
+            }, function(response){
+                // Cette fonction sera appelée en cas d'échec de la requête
+                console.log(response);
+            });
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
