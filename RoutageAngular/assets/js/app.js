c5fa4bb31c22dbae51e156c2ea2e3958084b4059
Cédric DANIEL <cdanie@cfcci53.fr>
2018-01-05 10:04:20 +0100

    Ajout du $routeParams

--- RoutageAngular/assets/js/app.js
+++ RoutageAngular/assets/js/app.js
@@ -1,14 +1,17 @@
 var app = angular.module('routage', ['ngRoute']);
 
 /* Déclaration d'un controller */
-app.controller('monController', function($scope){
-    
+app.controller('monController', function($scope,$routeParams){
+    $scope.id = $routeParams.id;
 });
 
 app.config(function($routeProvider){
     $routeProvider
         .when('/home',{
         templateUrl:'home.html',
+        controller:'monController'
+    }).when('/recipe/:id',{
+        templateUrl:'recipe.html',
         controller:'monController'
     }).when('/contact',{
         templateUrl:'contact.html',
